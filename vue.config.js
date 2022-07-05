b6356333df7399041706b736dd16a107ad6c2ee2
longyuhuan <2630819701@qq.com>
2022-07-05 22:38:48 +0800

    =

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ const themePath = path.join(__dirname, './src/theme.less')
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  // 部署应用包时的基本 URL，默认是 '/' 只有在一个域名上才能访问，将值改为 '' 或 './'，这样所有的资源都会被链接为相对路径，这样打出来的包可以在任意路径上访问
+  publicPath: '',
+  // 定制主题第二步
   css: {
     loaderOptions: {
       less: {
